010c583051a49fed405187640b196f845757165b
Oleksii Borshch <sergeevi44@gmail.com>
2022-05-26 18:23:47 +0300

    make first page render

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 import './css/main.css';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-import axios from 'axios';
 import fetchApi from './js/fetch-api';
+import makeMarkupFromTemplate from './templates/gallery.hbs';
 
 const refs = {
   form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadBtnClick);
 
 async function onFormSubmit(evt) {
   evt.preventDefault();
   const query = evt.currentTarget.elements.searchQuery.value;
   try {
-    await fetchApi(query);
+    const data = await fetchApi(query);
+    const images = data.data.hits;
+    console.log(images);
+    if (images.length === 0) {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.',
+      );
+    }
+    const markup = makeMarkupFromTemplate(images);
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+    refs.loadMoreBtn.style.display = 'block';
   } catch (error) {
     console.log(error);
+    Notiflix.Notify.failure('Something wrong');
   }
 }
+
+async function onLoadBtnClick() {}
--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,6 +1,7 @@
+import axios from 'axios';
+
 export default async function fetchApi(query) {
   const API_KEY = '27497064-4ae9b55936b7b9caff863013f';
-  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const result = await fetch(URL);
-  return await result.json();
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
+  return await axios.get(URL);
 }
