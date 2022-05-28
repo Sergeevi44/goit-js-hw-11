3da6923e19248c0bb25852bd4f7b818cdfcb3a15
Oleksii Borshch <sergeevi44@gmail.com>
2022-05-28 15:07:57 +0300

    clear main branch

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,17 +1,1 @@
 import axios from 'axios';
-
-// export default async function fetchApi(query) {
-//   const API_KEY = '27497064-4ae9b55936b7b9caff863013f';
-//   const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`;
-//   return await axios.get(URL);
-// }
-
-export default class FetchApi {
-  constructor() {}
-
-  async fetchImages(query) {
-    const API_KEY = '27497064-4ae9b55936b7b9caff863013f';
-    const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`;
-    return await axios.get(URL);
-  }
-}
